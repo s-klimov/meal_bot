ce9c03d86e76e66173c985078c334c6b94b7f0fe
Sergey Klimov <s.klimov@bk.ru>
2021-01-05 15:59:32 +0700

    Initial commit

--- app/handlers/private/mealtime.py
+++ app/handlers/private/mealtime.py
@@ -8,29 +8,31 @@ from app.loader import dp, db
 from app.models import Mealtime
 
 
-# TODO выводить количество приемов пищи за последние пять дней в порядке убывания дат, текущую дату показывать детально
+# TODO отформатировать строки вывода информации
 @dp.message_handler(Command("statistic", prefixes="!/"))
 async def command_statistic_handler(message: types.Message):
-    items = await(db.select([Mealtime.meal_date, db.func.count(Mealtime.meal_date)])
-                   .select_from(Mealtime)
-                   .where(Mealtime.user_id == message.from_user.id)
-                   .group_by(Mealtime.meal_date)
-                   .order_by(desc(Mealtime.meal_date))  # сортируем по убыванию дат от текущей
-                   .limit(5)    # Получаем первые 5 записей
-                   .gino
-                   .all())
+    daily_records = await(db.select([Mealtime.meal_date, db.func.count(Mealtime.meal_date)])
+                          .select_from(Mealtime)
+                          .where(Mealtime.user_id == message.from_user.id)
+                          .group_by(Mealtime.meal_date)
+                          .order_by(desc(Mealtime.meal_date))  # сортируем по убыванию дат от текущей
+                          .limit(5)  # Получаем первые 5 записей
+                          .gino
+                          .all())
 
-    today_items = await(Mealtime.query
-                        .where(and_(Mealtime.created_at >= datetime.now().date(),
-                                    Mealtime.user_id == message.from_user.id))
-                        .gino
-                        .all())
+    # daily_records = await Mealtime.query.where(Mealtime.user_id == message.from_user.id).group_by(Mealtime.meal_date).order_by(desc(Mealtime.meal_date)).limit(5).gino.first()
 
-    for item in items:
-        await message.answer(f"{str(item)}\n")
-        print(item[0], date.today())
-        if item[0] == date.today():
-            await message.answer("\n".join([str(foo.created_at) for foo in today_items]))
+    entries_today = await(Mealtime.query
+                          .where(and_(Mealtime.created_at >= datetime.now().date(),
+                                      Mealtime.user_id == message.from_user.id))
+                          .gino
+                          .all())
+
+    for record in daily_records:
+        await message.answer(f"{str(record)}\n")
+        print(record[0], date.today())
+        if record[0] == date.today():
+            await message.answer("\n".join([str(foo.created_at) for foo in entries_today]))
 
 
 @dp.message_handler(Command("add", prefixes="!/"))
@@ -48,6 +50,5 @@ async def add_mealtime_event(message: types.Message):
 
 @dp.message_handler(Command("remove", prefixes="!/"))
 async def del_mealtime_event(message: types.Message):
-
     # TODO реализовать удаление послденей записи за сегодня!
     await message.answer("Удалять последнюю запись я ещё не умею")
